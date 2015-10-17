ff06ebeef4d7cc3eb933ea35986745d3cc742e8d
DAni <daniru@gmail.com>
2015-10-17 15:54:03 +0100

    Initial commit

---
+++ quickstart_ts/src/app/app.ts
@@ -0,0 +1,10 @@
+// Typescript
+import {Component, bootstrap} from 'angular2/angular2'; 
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>My second Angular 2 App</h1>'
+})
+class AppComponent {}
+
+bootstrap(AppComponent);
